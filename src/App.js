e8ade1ddadb892b6281dfd4cfc46bf592cdb861b
yhee <yhee2075@naver.com>
2023-10-15 21:54:14 +0900

    Add: Footer 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MainPage from './pages/MainPage/MainPage';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <MainPage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-slate-700 text-white h-32 mt-16 flex items-center justify-around text-center">
+      Weplace © Copyright 2022. All Rights Reserved.
+    </div>
+  );
+};
+
+export default Footer;
