ef1307750adcb7e1e05fe1a877f3dd9bb451cbeb
yhee <yhee2075@naver.com>
2023-10-28 21:34:28 +0900

    Add: Header 로고 클릭 시 메인페이지로 이동

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <div className=" h-20 w-full p-2 border-b border-gray-200 bg-white fixed top-0">
       <div className="view_wrap flex justify-between h-full items-center">
-        <h1 className=" font-semibold text-4xl text-green-700">Weplace</h1>
+        <a href="/">
+          <h1 className=" font-semibold text-4xl text-green-700">Weplace</h1>
+        </a>
         <Button contants={'로그인'} />
       </div>
     </div>
