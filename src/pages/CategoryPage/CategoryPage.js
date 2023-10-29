0ae49f07101734d2da3632b74f4c11744d710114
yhee <yhee2075@naver.com>
2023-10-29 09:00:50 +0900

    Add: 카테고리페이지 리뷰리스트 추가

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -5,6 +5,7 @@ import API from '../../api/axios';
 const CategoryPage = () => {
   const [categoryList, setCategoryList] = useState([]);
   const [tagList, setTagList] = useState([]);
+  const [reviewList, setReviewList] = useState([]);
 
   const fetchCategory = async () => {
     const categorys = await axios.get(API.categoryfilters);
@@ -13,8 +14,12 @@ const CategoryPage = () => {
   };
   const fetchTagList = async () => {
     const categoryTags = await axios.get(API.categoryList);
+    const reivewData = categoryTags.data.data;
+    console.log('reivewData', reivewData);
+    setReviewList(reivewData);
+
     // 배열 하나로 합치기 ---- flatMap() => 한 단계 평탄화하여 새배열 생성
-    const tags = categoryTags.data.data.flatMap(e => e.tag);
+    const tags = categoryTags.data.data.flatMap(t => t.tag);
     setTagList(tags);
     // 합친 배열 중 중복 항목 제거
     const result1 = new Set(tags);
@@ -50,10 +55,35 @@ const CategoryPage = () => {
           {tagList.map((tag, idx) => (
             <button
               key={idx}
-              className="inline px-4 p-1 font-medium text-base bg-white rounded-full text-green-500 shadow-sm "
+              className="inline px-4 p-1 font-medium text-base bg-white rounded-full text-green-500 shadow-sm shadow-green-300 "
             >
               {`# ${tag}`}
             </button>
+          ))}
+        </div>
+      </div>
+      <div className="reviewList">
+        <div className="view_wrap grid md:grid-cols-3 grid-cols-2 gap-5 mt-4">
+          {reviewList.map(review => (
+            <div key={review.idx} className="flex">
+              <div className="bg-white rounded-md p-4 w-full shadow-md">
+                <div className="flex justify-between h-7 items-center">
+                  <div className="px-3 p-1 text-sm bg-green-500 rounded-full text-white shadow-sm">
+                    {review.category}
+                  </div>
+                  <div className=" text-4xl text-gray-500 hover:text-gray-400">♥️</div>
+                </div>
+                <div className=" font-extrabold text-xl">{review.name}</div>
+                <div className="flex gap-2">
+                  <div>{review.star}</div>
+                  <div>{review.review}</div>
+                  <div>{review.favorite}</div>
+                </div>
+                <div className="bg-green-100 rounded-md p-4">
+                  <div className="inline px-4 p-1 font-medium text-base bg-white rounded-full text-green-500 shadow-sm shadow-green-300">{`# ${review.tag}`}</div>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
       </div>
